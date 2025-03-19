cd8e798f593961d0d1d1b1d7fdbc4fce679c36bd
alanfzf <alandavidg13@gmail.com>
2025-03-19 15:13:33 -0600

    feat(misc): handle stuff

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,10 +1,10 @@
 import { exec } from "child_process"
 import { promisify } from "util"
 
-const exec = promisify(exec)
+const exec_await = promisify(exec)
 
 async function execPromise(command, opts) {
-  const { stdout, stderr } = await exec(command, opts)
+  const { stdout, stderr } = await exec_await(command, opts)
 
   return { stdout, stderr }
 }
